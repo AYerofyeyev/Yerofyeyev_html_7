010e0790521c4a8b314915226c31bcc05da3703a
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-05-17 12:43:13 +0300

    grunt is configured v2

--- gruntfile.js
+++ gruntfile.js
@@ -1,19 +1,85 @@
 module.exports = function(grunt) {
 
-    // 1. Вся настройка находится здесь
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  // 1. Вся настройка находится здесь
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
 
-        concat: {
-            // 2. Настройка для объединения файлов находится тут
+    sass: {
+      dist: {
+        files: {
+          "css/style.css": "css/style.scss"
         }
+      }
+    },
 
-    });
+    postcss: {
+      options: {
+        map: true, // inline sourcemaps
 
-    // 3. Тут мы указываем Grunt, что хотим использовать этот плагин
-    grunt.loadNpmTasks('grunt-contrib-concat');
+        processors: [
+          require('pixrem')(), // add fallbacks for rem units
+          require('autoprefixer')({browsers: ['last 4 versions']}), // add vendor prefixes
+          require('cssnano')() // minify the result
+        ]
+      },
+        dist: {
+          src: 'css/style.css'
+        }
+    },
 
-    // 4. Указываем, какие задачи выполняются, когда мы вводим «grunt» в терминале
-    grunt.registerTask('default', ['concat']);
+    concat: {
+      // 2. Настройка для объединения файлов находится тут
+      dist: {
+        src: [
+          "js/libs/*.js",
+          "js/main.js"
+        ],
+        dest: "js/script.js"
+      }
+    },
+
+    uglify: {
+      build: {
+        src: "js/script.js",
+        dest: "js/script.min.js"
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: "img/",
+          src: ["**/*.{png,jpg,gif}"],
+          dest: "img/build/"
+        }]
+      }
+    },
+
+    browserSync: {
+      bsFiles: {
+        src : 'css/*.css'
+      },
+      options: {
+        server: {
+            baseDir: "./"
+        }
+      }
+    }
+  });
+
+  // 3. Тут мы указываем Grunt, что хотим использовать этот плагин
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // require('load-grunt-tasks')(grunt);
+
+  // 4. Указываем, какие задачи выполняются, когда мы вводим «grunt» в терминале
+  grunt.registerTask("default", ["sass", "postcss", "concat", "uglify", "imagemin", "browserSync"]);
 
 };
